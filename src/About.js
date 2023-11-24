7031c62ea89bfc3ccffd8d5d4e95f38eddc54a45
LewisJohnsonIU <149400394+LewisJohnsonIU@users.noreply.github.com>
2023-11-24 08:58:52 +0100

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+function About() {
+  return (
+    <main className="container my-5">
+      <h1>About Us</h1>
+      <p>
+        We are a non-profit organization dedicated to creating transparency
+        around CO2 emissions globally.
+      </p>
+      <section id="mission">
+        <h2>Unsere Mission</h2>
+        <p>
+          Unsere Mission ist es, Bewusstsein und Verständnis für die
+          CO2-Problematik zu fördern. Wir möchten aufzeigen, wo und in welchem
+          Umfang Emissionen entstehen, und damit einen Anstoß für positive
+          Veränderungen im Umweltbewusstsein und in der Umweltpolitik geben. Wir
+          wollen es allen Menschen ermöglichen, genau zu erfahren, welche
+          Unternehmen und Länder wie viel CO² jährlich emittieren. Wir glauben,
+          dass Transparenz ein Schlüssel zur Veränderung ist und hoffen, durch
+          unsere Arbeit ein Bewusstsein für die Dringlichkeit des Klimaschutzes
+          zu schaffen.
+        </p>
+      </section>
+      <section id="our-task">
+        <h2>Unsere Aufgabe</h2>
+        <p>
+          Es ist unsere Aufgabe, Daten und Fakten über CO2-Ausstöße zugänglich
+          zu machen. Wir arbeiten daran, diese Informationen verständlich und
+          für jeden zugänglich aufzubereiten, um eine informierte Diskussion und
+          nachhaltige Entscheidungsfindung zu ermöglichen Wir verstehen die
+          Komplexität des Klimawandels und seine Auswirkungen auf unsere Welt,
+          und wir sind fest davon überzeugt, dass der Zugang zu verlässlichen
+          Informationen der erste Schritt zu positiven Veränderungen ist.
+        </p>
+      </section>
+      <section id="our-website">
+        <h2>Unsere Webseite</h2>
+        <p>
+          Unsere Webseite dient als zentrale Plattform für den Austausch und die
+          Verbreitung von Informationen über CO2-Emissionen. Sie bietet Zugang
+          zu aktuellen Daten, Forschungsergebnissen und bietet eine Grundlage
+          für Bildung und Handlung. Wir haben sorgfältig Daten gesammelt und
+          präsentieren sie in einer leicht verständlichen Tabelle. Die
+          Möglichkeit zur Sortierung und Filterung ermöglicht es jedem, die
+          Informationen nach seinen Bedürfnissen anzuzeigen.
+        </p>
+      </section>
+      <section id="our-commitment">
+        <h2>Unser Engagement</h2>
+        <p>
+          Wir engagieren uns für eine nachhaltige Zukunft und arbeiten eng mit
+          Wissenschaftlern, Unternehmen und politischen Entscheidungsträgern
+          zusammen, um Strategien zur Reduktion von CO2-Emissionen zu entwickeln
+          und umzusetzen. Unser Team setzt sich aus Fachleuten verschiedener
+          Disziplinen zusammen, und wir teilen alle die gleiche Leidenschaft für
+          Umweltschutz und Nachhaltigkeit. Wir sind entschlossen, unsere
+          Webseite kontinuierlich zu verbessern und aktuelle Daten
+          bereitzustellen, um das Bewusstsein für die Klimakrise zu schärfen.
+        </p>
+      </section>
+      <section id="join-us">
+        <h2>Kommen Sie an Bord</h2>
+        <p>
+          Wir laden Sie herzlich ein, unsere Webseite zu erkunden und sich über
+          die CO²-Emissionen von Unternehmen und Ländern zu informieren.
+          Gemeinsam können wir einen Unterschied machen. Ihre Unterstützung und
+          Ihr Engagement sind uns wichtig, und wir glauben fest daran, dass wir
+          durch Bildung und Transparenz die Welt zu einem besseren Ort machen
+          können.
+        </p>
+        <p>
+          Vielen Dank, dass Sie Teil unserer Mission sind. Zusammen können wir
+          den Klimawandel bekämpfen und eine nachhaltige Zukunft schaffen.
+        </p>
+      </section>
+    </main>
+  );
+}
+
+export default About;
